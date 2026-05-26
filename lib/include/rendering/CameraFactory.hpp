8659eac9cfef0b202a90b2b15bb2cc94408affef
Jakub Neruda <nerudaj@email.cz>
2026-05-26 20:43:21 +0200

    Update CameraFactory.hpp

--- lib/include/rendering/CameraFactory.hpp
+++ lib/include/rendering/CameraFactory.hpp
@@ -5,7 +5,16 @@
 class [[nodiscard]] CameraFactory final
 {
 public:
-    dgm::Camera RenderingEngine::createFullscreenCamera(
+    /**
+    * \brief Create fullscreen camera with a fixed resolution and aspect ratio
+    *
+    * When porting games to multiple systems / resolutions / aspect ratio, the
+    * renderer can show less / more than you need to. This function allows you
+    * to run the game at some fixed internal resolution, inserting black bars
+    * if the aspect ratio of the current device mismatches your desired aspect
+    * ratio.
+    */
+    static dgm::Camera createFullscreenCamera(
         const sf::Vector2f& currentResolution,
         const sf::Vector2f& desiredResolution);
 };
