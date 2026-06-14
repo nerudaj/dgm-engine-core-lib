17419b9cb7ae99d7790d01564664d0e20f6edebe
Jakub Neruda <nerudaj@email.cz>
2026-06-14 22:39:55 +0200

    Make EventQueue double-buffered to prevent reallocation fails

--- lib/include/misc/EventQueue.hpp
+++ lib/include/misc/EventQueue.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <array>
 
 template<class T>
 class [[nodiscard]] EventQueue final
@@ -9,21 +10,40 @@ public:
     template<class EventType, class... Args>
     void pushEvent(Args&&... args)
     {
-        events.template emplace_back<EventType>(
+        events[activeQueueIdx].template emplace_back<EventType>(
             EventType { std::forward<Args>(args)... });
     }
 
     template<class... Visitor>
     void processEvents(Visitor&&... visitors) // NOLINT
     {
-        for (size_t idx = 0; idx < events.size(); ++idx)
+        /**
+         * Event queue is double-buffered to prevent reallocation issues
+         * While one buffer is used for reading, the other is used for writing.
+         *
+         * After finishing reading the current buffer, the other one is checked
+         * whether it is empty and if not, they are swapped.
+         */
+        while (!events[activeQueueIdx].empty())
         {
-            (std::visit(visitors, events[idx]), ...); // NOLINT
+            activeQueueIdx = 1 - activeQueueIdx;
+            processEvents(events[1 - activeQueueIdx], visitors...);
         }
-
-        events.clear();
     }
 
 private:
-    std::vector<T> events;
+    template<class... Visitor>
+    void processEvents(std::vector<T>& queue, Visitor&&... visitors) // NOLINT
+    {
+        for (size_t idx = 0; idx < queue.size(); ++idx)
+        {
+            (std::visit(visitors, queue[idx]), ...); // NOLINT
+        }
+
+        queue.clear();
+    }
+
+private:
+    uint8_t activeQueueIdx = 0;
+    std::array<std::vector<T>, 2u> events;
 };
